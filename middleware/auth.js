e5b0abe7fceb1b5a24ae76b5e18db5f5f45f4aef
mehtab39 <mehtabgill122@gmail.com>
2025-06-17 21:28:19 +0530

    feat: bypass token if query has id and roleid

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,17 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader?.split(' ')[1];
 
+    const id = req.query.userId;
+    const role_id = req.query.roleId;
+
+    if (id && role_id) {
+        req.userContext = {
+            id, role_id
+        };
+        next();
+        return;
+    }
+
     if (!token) return res.sendStatus(401);
 
     jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
